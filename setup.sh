7d6688e4bc5b8decc9e851c91dfa385f86cc7caf
Simon M <git@simon.marache.net>
2021-09-25 20:51:59 +0000

    Update setup to configure an instance from scratch

--- setup.sh
+++ setup.sh
@@ -2,10 +2,31 @@
 
 cd "$(dirname "$0")"
 
+git submodule init
+
+### Install docker and packages
+sudo apt-get -y remove docker docker-engine docker.io containerd runc snapd
+sudo apt -y autoremove
+
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release tmux vim htop
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get -y dist-upgrade
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose
+#### End of package installation
+
 sudo docker volume create --name=bibin_data
 sudo docker volume create --name=caddy_data
 sudo docker volume create --name=caddy_config
 sudo docker volume create --name=wikijs_data
 
 sudo docker-compose build
-sudo docker run -v bibin_data:/data --entrypoint /bin/bash --user root prod_bibin -c "chown -R nobody /db"
+
+sudo docker run -v bibin_data:/db --entrypoint /bin/bash --user root prod_bibin -c "chown -R nobody /db"
+sudo docker-compose up -d
+./install_cron_backup.sh
