0cdbe0399325f66e0a2be7b9ef42b1508438d080
Simon M <git@simon.marache.net>
2021-11-30 22:34:42 +0000

    Auto restart containers and improve build time

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$0")"
 git pull
 git submodule update --init --recursive
 (cd ./marache.net/ && hugo)
-sudo docker-compose build
+sudo docker-compose build --pull --parallel
 sudo docker-compose down
 sudo docker-compose up -d
