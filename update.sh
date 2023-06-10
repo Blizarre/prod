634de70226e73da79f9e6356171b13948e23fcda
Simon M <git@simon.marache.net>
2023-06-10 13:42:35 +0100

    Remove marache.net (gh pages)

--- update.sh
+++ update.sh
@@ -4,7 +4,6 @@ cd "$(dirname "$0")"
 
 git pull
 git submodule update --init --recursive
-(cd ./marache.net/ && hugo)
 sudo docker-compose build --pull --parallel
 sudo docker-compose down
 sudo docker-compose up -d
