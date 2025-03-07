b9bde682c62debe1834c0cecf11523e870e6a95d
Simon M <git@simon.marache.net>
2025-03-07 22:12:37 +0000

    Migrated off AWS

--- backup_restore/backup.sh
+++ backup_restore/backup.sh
@@ -4,18 +4,20 @@ set -euxo pipefail
 
 ACTION=${1-}
 
+mkdir -p ~/.ssh
+cp -r /ssh_host/* ~/.ssh/
+
 if [[ "$ACTION" = "backup" ]]; then
     pg_dumpall --clean "--host=$POSTGRES_HOST" "--username=$POSTGRES_USER" | bzip2 > wiki_data.bz2
-    aws s3 cp wiki_data.bz2 "s3://$S3_BUCKET/wiki_backup_$(date +"%Y-%m-%d").bz2"
-    aws s3 cp wiki_data.bz2 "s3://$S3_BUCKET/wiki_backup_latest.bz2"
+    scp wiki_data.bz2 hetzner:wiki_backup_$(date +"%u").bz2
+    scp wiki_data.bz2 hetzner:wiki_backup_latest.bz2
 
     sqlite3 "$SQLITE_FILE" ".backup 'bibin_data.sqlite3'"
     bzip2 bibin_data.sqlite3
-    aws s3 cp bibin_data.sqlite3.bz2 "s3://$S3_BUCKET/bibin_backup_$(date +"%Y-%m-%d").bz2"
-    aws s3 cp bibin_data.sqlite3.bz2 "s3://$S3_BUCKET/bibin_backup_latest.bz2"
-
+    scp bibin_data.sqlite3.bz2 hetzner:bibin_backup_$(date +"%u").bz2
+    scp bibin_data.sqlite3.bz2 hetzner:bibin_backup_latest.bz2
 elif [[ "$ACTION" = "restore" ]]; then
-    aws s3 cp "s3://$S3_BUCKET/wiki_backup_latest.bz2" - | \
+    ssh hetzner cat wiki_backup_latest.bz2 | \
         bunzip2 | \
         psql "--host=$POSTGRES_HOST" "--username=$POSTGRES_USER" postgres
 
@@ -25,7 +27,7 @@ elif [[ "$ACTION" = "restore" ]]; then
     fi
     # Remove any temporary file that might be there
     rm -f "$SQLITE_FILE"-*
-    aws s3 cp "s3://$S3_BUCKET/bibin_backup_latest.bz2" - | \
+    ssh hetzner cat bibin_backup_latest.bz2 | \
         bunzip2 > "$SQLITE_FILE"
 else
     echo "Nothing to do"
